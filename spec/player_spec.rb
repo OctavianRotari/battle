5e6ccbe2523ffc36597b71e70c24d2f3ec24bcc2
samover <samuel.overloop@gmail.com>
2015-10-30 12:15:56 +0000

    unit testing for attack method Player

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,24 @@
 require 'player'
 
 describe Player do
-  subject(:player) { described_class.new('Raphael') }
+  subject(:player_one) { described_class.new('Raphael') }
+  subject(:player_two) { described_class.new('Leonardo') }
 
   describe '#name' do
     it 'returns the name of the player' do
-      expect(player.name).to eq 'Raphael'
+      expect(player_one.name).to eq 'Raphael'
+    end
+  end
+
+  describe '#hitpoints' do
+    it 'shows the hitpoints of the player' do
+      expect(player_one.hitpoints).to eq Player::DEFAULT_HP
+    end
+  end
+
+  describe '#attack' do
+    it 'player 1 attacks player 2 and reduces HP by 10' do
+      expect{player_one.attack player_two}.to change{player_two.hitpoints}.by(-10)
     end
   end
 end
