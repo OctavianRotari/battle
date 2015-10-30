a1f31fb9cdccc047823c173aaf6d75d933255055
samover <samuel.overloop@gmail.com>
2015-10-30 10:48:55 +0000

    name form and beginning of game

--- spec/features/user_name_spec.rb
+++ spec/features/user_name_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 feature "adding player names" do
   scenario 'by filling in the form' do
     visit('/')
-    fill_in('player_1', :with => "Wolverine" )
-    fill_in('player_2', :with => "Spiderman" )
+    fill_in('player_1', with: "Wolverine" )
+    fill_in('player_2', with:  "Spiderman" )
     click_button("May the force be with you")
     expect(page).to have_content("Wolverine vs Spiderman")
   end
