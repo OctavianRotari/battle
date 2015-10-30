ca867bdee1cb659017463977a8e87dcc4bf97d69
samover <samuel.overloop@gmail.com>
2015-10-30 11:55:56 +0000

    implemented player class using EVIL global variable

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require_relative './lib/player'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -8,20 +9,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player_one = Player.new(params[:player_1_name])
+    $player_two = Player.new(params[:player_2_name])
     redirect :play
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_one.name
+    @player_2_name = $player_two.name 
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_one.name
+    @player_2_name = $player_two.name 
     erb :attack
   end
 
