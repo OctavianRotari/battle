6592d7b3df92bda443970d3b298912b3ceb77edb
octavianrotari <octavianrotari@gmail.com>
2015-10-30 12:07:35 +0000

    write spec test for continue battle button

--- spec/features/attack_player_spec.rb
+++ spec/features/attack_player_spec.rb
@@ -6,4 +6,11 @@ feature 'Attack another player' do
     click_button('Attack')
     expect(page).to have_content('Wolverine attacked Spiderman')
   end
+
+  scenario 'When player 1 attacks player 2 HP goes down' do
+    sign_in_and_play
+    click_button('Attack')
+    click_button('Continue to battle')
+    expect(page).to have_content('Spiderman: 50/60HP')
+  end
 end
